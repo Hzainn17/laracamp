6727df19133c2b71865b8f0c30c8c3b9f5246871
Hzainn17 <hilmazain17@gmail.com>
2024-03-01 02:18:10 +0700

    Discount management admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-8 offset-2">
+            <div class="col-8 offset-2 mt-4">
                 <div class="card">
                     <div class="card-header">
                         My Camps
